83500e253c1f79a751a7d65b457f789d0ec74424
Nizzzzu <157376768+Nizzzzu@users.noreply.github.com>
2024-05-09 05:46:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //═══════[Required Variables]════════\\
 global.audio= "" ;  
-global.video= "https://telegra.ph/file/db3a5c86c0e3dd11e0071.mp4" ;
+global.video= "https://telegra.ph/file/af7d24f33e91da478b311.png" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
 global.email ="ngoyaibraah@gmail.com"
@@ -106,7 +106,7 @@ global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel
 global.scan = "https://black-panther-scanner-871df7205958.herokuapp.com/";
 
 
-global.SESSION_ID = process.env.SESSION_ID ||  "" ; // PUT your SESSION_ID 
+global.SESSION_ID = process.env.SESSION_ID ||  "PANTHER;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiS0pmK2pBTUV0WGNQdGZUQWJTK2RBeWh5ZW9BTE42Yks3cUtMWVZkMnJuYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUjQxTTBVbnBCVmdKVkwzcUZreUZqcWp1WVhzV3NlMDRUcEJWRXRpQStRVT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJxT2pOcjlVOWxYV2czRklZZnZzQ0c3TGNxT3UrRWNEN3dDcGsvMCtqRVgwPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJkb21ud216MmQ5TFZvVnR3cjl4SDRQTUg1T2ZYRFp0eDM5NHlyT2tFNVdJPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFBZlh5eTZBaXZickhBRGdLQTFHOWZMdmR1YVRqTjE0VnFkZzkyMitiM0U9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjFidG93bFQrR1p2S01IeVVhZVJPQXZOb3lXd2N3UjB5RE16V29ZVDl2U289In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSUR0TkVyNENOYTNrVFRPUmpYV0Y1RTdEa2tYVlpWUnFac0lyaC8wY3dsMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOVlKVnN6Y0ZzTEt2TWlid0oxTUtFUEZwWUpSQ0FvenBTVFhxODN0ZUlqbz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InlyL1FJUGpNRGlZOXd0UTVzZy9ycEVPZk5IaUlyY0J2S0lxaHVLUTU1eDFmQnhFOXFxcSs5a255dW9DQnBCNjNXMHRmb3hUZTdOdkwzNHgxVklETGh3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTcsImFkdlNlY3JldEtleSI6Ijc1ejBRVEdZRUFqcEtsUU0za2pqV3NIYysxeWR4djYwSUNSNHZCYTlMUlk9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImlkaFhtNkFnVERlUW9ySjF1bV9jYkEiLCJwaG9uZUlkIjoiOGQ2YjZjMmUtNzQ5Ny00MjJjLTk5MjEtMDRlYzNiYWQzMTdhIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IllCVUQwcldUbUtBajBCVXUrS1JNWWlVcmhIYz0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJwcVowS2FKZE1BZzd2NG9zaGVPelNKMjExazA9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiMVoxRUU2VFciLCJtZSI6eyJpZCI6Ijk0NzYxNjUyMTMyOjIyQHMud2hhdHNhcHAubmV0IiwibmFtZSI6Ik5penp1LWlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNMV1Yxc1FERU5pZzhMRUdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJieGQ0YU4zazE1WUV2dnRoNW5DcnBqN2N5RjRma0dSc0doQmNWYW9NMjEwPSIsImFjY291bnRTaWduYXR1cmUiOiJxdzZueCtLYjhnaTZKODlpSWMyQlM3RFRKSkhDS2VDd2ZIYjlhMm1ERW85TFhBWGgwRmI5WHQrYWQ5cW1OMFdpRTc2QTlZdXM5bWRBc2VmSFQ1UTRCUT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiSGhMK2s2REJQTmxWNmN2YnZRczN0RXZrS2JUc2dwMGNISmJ5R2poZlBqVmNDVys4YlV0NHdKakN1ZU1CM2lCbjNrYUNWc1JsVGFncGhRTCsvZUJxaWc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5NDc2MTY1MjEzMjoyMkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJXOFhlR2pkNU5lV0JMNzdZZVp3cTZZKzNNaGVINUJrYkJvUVhGV3FETnRkIn19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzE1MjEyMzg4fQ==" ; // PUT your SESSION_ID 
 
 
 module.exports = {
@@ -120,7 +120,7 @@ module.exports = {
  
   author : process.env.PACK_AUTHER|| "Pᵒʷᵉʳᵉᵈ ᵇʸ Iᵇʳᵃʰⁱᵐ Aᵈᵃᵐˢ",
   packname: process.env.PACK_NAME || "Pᵒʷᵉʳᵉᵈ ᵇʸ Iᵇʳᵃʰⁱᵐ Aᵈᵃᵐˢ",
-  botname : process.env.BOT_NAME  || "𝘽𝙇𝘼𝘾𝙆 𝙋𝘼𝙉𝙏𝙃𝙀𝙍 𝙈𝘿",
+  botname : process.env.BOT_NAME  || "Nizzu Freezy 𝙈𝘿",
   ownername:process.env.OWNER_NAME|| "𝑰𝒃𝒓𝒂𝒉𝒊𝒎 𝑨𝒅𝒂𝒎𝒔",
 
 
